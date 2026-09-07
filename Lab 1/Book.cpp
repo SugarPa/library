389535d44b613fce25116a569a675f5befafee22
Arkal <Arkal@DESKTOP-OKTMDJC>
2026-09-07 18:49:32 +0300

    Добавьте файлы проекта.

---
+++ Lab 1/Book.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+class Book {
+public:
+	std::string name;
+	std::string identifier;
+	std::string year;
+	std::string publisher;
+	unsigned short number;
+};
